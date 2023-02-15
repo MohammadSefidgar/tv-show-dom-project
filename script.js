d680acd03c6d5248c6cb052a5ba2c7bab9f7c818
MohammadSefidgar <108987757+MohammadSefidgar@users.noreply.github.com>
2023-02-15 21:47:34 +0000

    TV-show-level 400

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // An array of objects Containing the list of all the episodes
+let allShows = getAllShows();
 let allEpisodes;
 const input = document.querySelector(".searchBar");
+const SHOW = "show";
+const EPISODE = "episode";
 
 /*
  * Role - To add leading zeroes to a number
@@ -13,8 +16,42 @@ const input = document.querySelector(".searchBar");
 function zeroPad(num, places) {
     // calculate the number of zeroes that need to be prepended to num
     let zero = places - num.toString().length + 1;
-
     return Array(+(zero > 0 && zero)).join("0") + num;
+}
+
+/*
+ * Role - To create an HTML block for a single show
+ * Parameter - A single show passed as a parameter
+ * Returns - An HTML div element that contains the show data,
+ *  enclosed in HTML tags.
+ * Result - Creates an HTML block with all the needed show data.
+ *  This block can then be appended to the DOM to display the show.
+ */
+function createShowBlock(show) {
+    // create the div element that will contain all the episode info
+    let showBlock = document.createElement("div");
+    // console.log("create show block");
+    // console.log(show);
+    /*
+     assign the episodeBlock a class "episodeBlock",
+     this class is then used to apply CSS styling
+    */
+    showBlock.className = "showBlock";
+
+    /*
+     populate the innerHTML variable with the episode title, image and summary
+    */
+    let imgSrc = show.image.medium || "";
+
+    let innerHTML = `<h2 class="showTitle">${show.name}</h2>
+    <hr>
+    <img src="${imgSrc}">
+    <h3 class="summaryTitle">Summary:</h3>
+    <div class="summaryText">${show.summary}</div>
+    <a class="showLink" href=${show.url} target="_blank">More...</a>`;
+
+    showBlock.innerHTML = innerHTML;
+    return showBlock;
 }
 
 /*
@@ -28,37 +65,35 @@ function zeroPad(num, places) {
 function createEpisodeBlock(episode) {
     // create the div element that will contain all the episode info
     let episodeBlock = document.createElement("div");
-
     /*
      assign the episodeBlock a class "episodeBlock",
      this class is then used to apply CSS styling
     */
     episodeBlock.className = "episodeBlock";
-
     /*
      populate the innerHTML variable with the episode title, image and summary
     */
+    let imgSrc = episode.image.medium || "";
+
     let innerHTML = `<h2 class="episodeTitle">${episode.name}
     <span class="episodeCode">
-    S${zeroPad(episode.season, 2)}E${zeroPad(episode.number, 3)}
+    S${zeroPad(episode.season, 2)} E${zeroPad(episode.number, 2)}
     </span>
     </h2>
     <hr>
-    <img src="${episode.image.medium}">
+    <img src="${imgSrc}">
     <h3 class="summaryTitle">Summary:</h3>
     <div class="summaryText">${episode.summary}</div>
     <a class="episodeLink" href=${episode.url} target="_blank">More...</a>`;
-
     episodeBlock.innerHTML = innerHTML;
     return episodeBlock;
 }
-
 /*
  * Role - To display the episode list.
  * Parameter - Takes the array of episodes (which are objects) as a parameter.
  * Result - Displays the episodes on the page using DOM manipulation.
  */
-function makePageForEpisodes(episodeList) {
+function render(list, type) {
     // get the root element that should contain the episodes, from the DOM
     const rootElem = document.getElementById("root");
 
@@ -68,28 +103,33 @@ function makePageForEpisodes(episodeList) {
     */
     rootElem.innerHTML = "";
 
-    episodeList.forEach((episode) => {
+    list.forEach((element) => {
+        let htmlBlock;
         /*
          create the HTML episode tile for each episode in the list
         */
-        let episodeBlock = createEpisodeBlock(episode);
+        if (type === SHOW) {
+            htmlBlock = createShowBlock(element);
+        } else if (type === EPISODE) {
+            htmlBlock = createEpisodeBlock(element);
+        }
+
         /*
      then append each episode to the root element (i.e. to the DOM)
     */
-        rootElem.appendChild(episodeBlock);
+        rootElem.appendChild(htmlBlock);
     });
 }
-
 
 /*
  * Role -
  * Parameter - Takes the array of episodes (which are objects) as a parameter.
  * Result - Displays the episodes on the webpage.
  */
-function render(episodeList) {
-    makePageForEpisodes(episodeList);
-    // createEpisodeDropdown(episodeList);
-}
+// function render(episodeList) {
+//   makePageForEpisodes(episodeList);
+//   // createEpisodeDropdown(episodeList);
+// }
 
 /*
  * Role - Filter the episodes based on the characters typed in the search bar
@@ -104,7 +144,6 @@ function searchEpisodes() {
     let numOfEpisodes = document.getElementById("numOfEpisodes");
     let searchString = input.value.toLowerCase();
     let searchResult = [];
-
     if (searchString === "") {
         /*
          If the search string is empty, no search is made.
@@ -128,15 +167,15 @@ function searchEpisodes() {
         */
         numOfEpisodes.innerHTML = `Displaying : ${searchResult.length} / ${allEpisodes.length} episodes`;
     }
-    //console.log("this is the search term : " + searchString);
-    //console.log("search result");
-    //console.log(searchResult);
-
+    // console.log("this is the search term : " + searchString);
+    // console.log("search result");
+    // console.log(searchResult);
     /*
-   Render the webpage with the filtered search results
-  */
-    render(searchResult);
+     Render the webpage with the filtered search results
+    */
+    render(searchResult, EPISODE);
 }
+
 /*
  * Role - Event listener for the episode dropdown
  * Parameter - event
@@ -144,24 +183,25 @@ function searchEpisodes() {
  * Result - Render the selected episode on the screen i.e. on the DOM
  */
 function episodeDropdownEventListener(event) {
-    //console.log(event.target.value);
-
+    // console.log(event.target.value);
     /*
      There are two ways in which the dropdown can render a single episode
      on the screen. Only one of the two methods should be uncommented at a time.
     */
-
     /*
      First method -
     */
-    // if (event.target.value === "All episodes") {
-    //   render(allEpisodes);
-    // } else {
-    //   let episodeToDisplay = episodeList.filter(
-    //     (episode) => episode.name === event.target.value
-    //   );
-    //   render(episodeToDisplay);
-    // }
+
+
+    if (event.target.value === "All episodes") {
+        render(allEpisodes, EPISODE);
+    } else {
+        let episodeToDisplay = allEpisodes.filter(
+            (episode) => episode.name === event.target.value
+        );
+        console.log(episodeToDisplay);
+        render(episodeToDisplay, EPISODE);
+    }
 
     /*
      Second Method - In this method the search bar is used to render the selected
@@ -172,12 +212,12 @@ function episodeDropdownEventListener(event) {
      and that no episode title is a substring of another title.
      And it does not occur in the episode summary.
     */
-    if (event.target.value === "All episodes") {
-        input.value = "";
-    } else {
-        input.value = event.target.value;
-    }
-    searchEpisodes();
+    // if (event.target.value === "All episodes") {
+    //   input.value = "";
+    // } else {
+    //   input.value = event.target.value;
+    // }
+    // searchEpisodes();
 }
 
 /*
@@ -189,12 +229,10 @@ function episodeDropdownEventListener(event) {
  */
 function createEpisodeDropdown(episodeList) {
     const episodeDropdown = document.getElementById("episodeDropDownList");
-
     /*
      delete the previously created options before proceeding
     */
     episodeDropdown.innerHTML = "";
-
     /*
      the first option in the dropdown is "All episodes"
      so that the user has a way to see all episodes.
@@ -204,7 +242,6 @@ function createEpisodeDropdown(episodeList) {
     let firstOption = document.createElement("option");
     firstOption.textContent = "All episodes";
     episodeDropdown.add(firstOption);
-
     /*
      create <option> for each episode, add its text and value,
      then append it to <select>
@@ -212,44 +249,129 @@ function createEpisodeDropdown(episodeList) {
     episodeList.forEach((episode) => {
         let dropDownOption = document.createElement("option");
         dropDownOption.textContent = `S${zeroPad(episode.season, 2)}E${zeroPad(
-        episode.number,
-        2
-      )}
-        - ${episode.name}`;
-
+      episode.number,
+      2
+    )}
+      - ${episode.name}`;
         dropDownOption.value = episode.name;
-
         episodeDropdown.add(dropDownOption);
     });
-
     /*
      add the eventListener to <select> i.e. the episode dropdown
     */
     episodeDropdown.addEventListener("change", episodeDropdownEventListener);
 }
+
+/*
+ * Role - Event listener for the show dropdown
+ * Parameter - event
+ * Returns - Nothing
+ * Result - Render all the episodes of the selected show
+ * on the screen i.e. on the DOM
+ */
+async function showDropdownEventListener(event) {
+    // console.log(event.target.value);
+    /*
+     First method -
+    */
+    console.log(event.target.value);
+    if (event.target.value === "All shows") {
+        render(allShows, SHOW);
+        allEpisodes = [];
+    } else {
+        let episodesToDisplay = await fetchAllEpisodes(event.target.value);
+        await render(episodesToDisplay, EPISODE);
+        createEpisodeDropdown(episodesToDisplay);
+        allEpisodes = episodesToDisplay;
+    }
+}
+
+/*
+ * Role -
+ * Parameter -
+ * Returns -
+ * Result -
+ */
+function createShowDropdown(showList) {
+    const showDropdown = document.getElementById("showDropDownList");
+
+    /*
+     delete the previously created options before proceeding
+    */
+    showDropdown.innerHTML = "";
+
+    /*
+     the first option in the dropdown is "All episodes"
+     so that the user has a way to see all episodes.
+     Create <option> for first option, add its value
+     and then add it to <select>
+    */
+    let firstOption = document.createElement("option");
+    firstOption.textContent = "All shows";
+    showDropdown.add(firstOption);
+
+    /*
+     create <option> for each episode, add its text and value,
+     then append it to <select>
+    */
+    showList.forEach((show) => {
+        let dropDownOption = document.createElement("option");
+        dropDownOption.textContent = show.name;
+
+        dropDownOption.value = show.id;
+
+        showDropdown.add(dropDownOption);
+    });
+
+    /*
+     add the eventListener to <select> i.e. the episode dropdown
+    */
+    showDropdown.addEventListener("change", showDropdownEventListener);
+}
+
 /*
  * Role - To fetch episodes using the Fetch API
  * Parameter - None
  * Returns - An array of episodes (i.e. an array objects).
  */
-async function fetchAllEpisodes() {
-    let promise = await fetch("https://api.tvmaze.com/shows/82/episodes");
+async function fetchAllEpisodes(showID) {
+    let promise = await fetch(`https://api.tvmaze.com/shows/${showID}/episodes`);
     let episodes = await promise.json();
     // console.log(episodes);
     return episodes;
 }
-
 /*
  * Role - To do the page setup.
  * Parameter - None
  * Result - Completes the page setup and called on window load.
  */
 async function setup() {
-    allEpisodes = await fetchAllEpisodes();
-    render(allEpisodes);
+    // allEpisodes = await fetchAllEpisodes(82);
+    allEpisodes = [];
+
+    /*
+     Sort the shoList by name before creating the htmlBlock for each show
+    */
+    allShows.sort(function(a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        // names must be equal
+        return 0;
+    });
+
+    // console.log(allShows);
+
+    render(allShows, "show");
     createEpisodeDropdown(allEpisodes);
+    createShowDropdown(allShows);
     input.addEventListener("input", searchEpisodes);
-    fetchAllEpisodes();
+    // fetchAllEpisodes();
 }
 
 window.onload = setup;
